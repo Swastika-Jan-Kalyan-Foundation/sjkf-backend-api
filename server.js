0e4d379913d8e84a0895564c4fde4b7ce02d3d6a
Gyan <gyan0304@gmail.com>
2026-04-25 19:51:54 -0400

    cors setup for vercel

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://swastikajankalyanfoundation.netlify.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.use(
   cors({
     origin: "https://swastikajankalyanfoundation.netlify.app",
