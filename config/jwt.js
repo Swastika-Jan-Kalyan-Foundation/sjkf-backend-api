8e2cfd1d39710ac7dbb4a94309516a64e58a6b3e
Gyan Shresth(Kunal) <gyan0304@gmail.com>
2026-08-07 23:55:23 +0530

    feat: jwt generation

---
+++ config/jwt.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const SESSION_HOURS = Number(process.env.SESSION_HOURS || 6);
+const SESSION_SECONDS = SESSION_HOURS * 60 * 60;
+
+function signToken(user) {
+  return jwt.sign(
+    {
+      sub: user._id.toString(),
+      tv: user.tokenVersion, 
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: SESSION_SECONDS }
+  );
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, process.env.JWT_SECRET);
+}
+
+module.exports = { signToken, verifyToken, SESSION_SECONDS };
